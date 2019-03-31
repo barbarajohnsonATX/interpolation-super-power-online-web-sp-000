2d6f8ee87a62f2f7916595630b91e2f946fe760f
Barbara Johnson <johnson.barbara@gmail.com>
2019-03-31 00:46:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
   puts "R: #{color[0]}"
-  puts "O: #{color[0]}"
+  puts "O: #{color[1]}"
   puts "R: #{color[0]}"
   puts "R: #{color[0]}"
   
