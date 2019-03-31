9a7ccab87a7ff6fafcdcd454238b844a87fd7a00
Barbara Johnson <johnson.barbara@gmail.com>
2019-03-31 00:47:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,9 @@ def display_rainbow(color)
   puts "R: #{color[0]}"
   puts "O: #{color[1]}"
   puts "Y: #{color[2]}"
-  puts "R: #{color[0]}"
+  puts "G: #{color[3]}"
+  puts "B: #{color[4]}"
+  puts "I: #{color[5]}"
+  puts "V: #{color[6]}"
+end
   
